b9ae46fe8719896bffc626a13aeeb8c0aeb4dfc9
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-06-14 17:59:52 -0300

    inserindo_autor

--- src/br/com/wesleyeduardo/livraria/bean/LivroBean.java
+++ src/br/com/wesleyeduardo/livraria/bean/LivroBean.java
@@ -1,17 +1,46 @@
 package br.com.wesleyeduardo.livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.wesleyeduardo.livraria.dao.DAO;
+import br.com.wesleyeduardo.livraria.modelo.Autor;
 import br.com.wesleyeduardo.livraria.modelo.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
 	private Livro livro = new Livro();
 
 	public Livro getLivro() {
 		return livro;
+	}
+
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+
+	private Integer autorId;
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+
+	}
+
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
 	}
 
 	public void gravar() {
