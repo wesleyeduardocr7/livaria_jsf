16e47c8f23359b0c403702a319eff71e8aac6fd8
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-06-14 10:29:03 -0300

    separando_responsabilidades

--- src/br/com/wesleyeduardo/LivroBean.java
+++ src/br/com/wesleyeduardo/LivroBean.java
@@ -5,47 +5,15 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 public class LivroBean {
 
-	private String titulo;
-	private String isbn;
-	private double preco;
-	private String dataLancamento;
+	private Livro livro = new Livro();
+	
 
-	public String getTitulo() {
-		return titulo;
+	public Livro getLivro() {
+		return livro;
+	}	
+		
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo());
 	}
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getIsbn() {
-		return isbn;
-	}
-
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
-
-	public double getPreco() {
-		return preco;
-	}
-
-	public void setPreco(double preco) {
-		this.preco = preco;
-	}
-
-	public String getDataLancamento() {
-		return dataLancamento;
-	}
-
-	public void setDataLancamento(String dataLancamento) {
-		this.dataLancamento = dataLancamento;
-	}
-
-	 public void gravar() {
-	        System.out.println("Gravando livro " + this.titulo);
-	    }
-	 
-	 
-	 
 }
