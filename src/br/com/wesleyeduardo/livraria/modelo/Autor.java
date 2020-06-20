58687f9c2b143d43567d4b76182c1f03726834de
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-06-20 11:43:59 -0300

    inserindo E-mail utilizando inputs do HTML 5

--- src/br/com/wesleyeduardo/livraria/modelo/Autor.java
+++ src/br/com/wesleyeduardo/livraria/modelo/Autor.java
@@ -11,6 +11,16 @@ public class Autor {
 	@GeneratedValue
 	private Integer id;
 	private String nome;
+	private String email;
+	
+		
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 	public String getNome() {
 		return nome;
